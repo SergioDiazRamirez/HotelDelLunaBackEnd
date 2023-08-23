0985634dbcd4b47fc226207642aac24d6bd3cd4a
Sergio Diaz <sergio.diaz-ramirez@t-systems.com>
2023-08-23 16:32:02 +0200

    Spring boot initializer

---
+++ src/main/java/com/sergiodr/hoteldelLuna/HoteldelLunaApplication.java
@@ -0,0 +1,13 @@
+package com.sergiodr.hoteldelLuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteldelLunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoteldelLunaApplication.class, args); //rt
+	}
+
+}
---
+++ src/test/java/com/sergiodr/hoteldelLuna/HoteldelLunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiodr.hoteldelLuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoteldelLunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
